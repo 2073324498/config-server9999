e508dc347ab2dc477ab425cae0ea44cec4fc7169
一个刘先生 <2073324498@qq.com>
2021-12-22 22:18:47 +0800

    提交配置

--- src/main/java/com/config/liu/ConfigServer9999Application.java
+++ src/main/java/com/config/liu/ConfigServer9999Application.java
@@ -1,9 +1,8 @@
-package com.eureka.eurekaserver8090;
+package com.config.liu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
--- src/test/java/com/config/liu/ConfigServer9999ApplicationTests.java
+++ src/test/java/com/config/liu/ConfigServer9999ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eureka.eurekaserver8090;
+package com.config.liu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
